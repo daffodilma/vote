746f80a34167ff9c745f9bdc56590edc936641d1
flower <flower@admindeMacBook-Air.local>
2023-02-07 11:04:25 +0800

    投票页面初搭建

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <voting msg="Welcome to Your Vue.js App"/>
+    <voting msg="A Simple Hello World Voting Application"/>
   </div>
 </template>
 
--- src/components/VotingPage.vue
+++ src/components/VotingPage.vue
@@ -1,7 +1,34 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    
+ <div id="address"></div>
+ <div class="table-responsive">
+  <table class="table table-bordered">
+   <thead>
+    <tr>
+     <th>Candidate</th>
+     <th>Votes</th>
+    </tr>
+   </thead>
+   <tbody>
+    <tr>
+     <td>Lucy</td>
+     <td id="candidate-1"></td>
+    </tr>
+    <tr>
+     <td>Lancy</td>
+     <td id="candidate-2"></td>
+    </tr>
+    <tr>
+     <td>Wayne</td>
+     <td id="candidate-3"></td>
+    </tr>
+   </tbody>
+  </table>
+  <div id="msg"></div>
+ </div>
+ <input type="text" id="candidate" />
+ <a href="#" onclick="voteForCandidate()" class="btn btn-primary">Vote</a>
   </div>
 </template>
 
