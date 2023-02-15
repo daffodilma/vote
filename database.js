fd9b27bc9fa5abd2f7758ffe01aaa6740c1a3ecb
flower <flower@admindeMacBook-Air.local>
2023-02-15 14:19:19 +0800

    成功运行

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var conn = mysql.createConnection({
-  host: 'sh-cynosdbmysql-grp-7224u8cw.sql.tencentcdb.com',
-  port:'21364', // assign your host name
-  user: 'vote_dapp_dev',      //  assign your database username
-  password: 'qk*$R%1H',      // assign your database password
-  database: 'vote_dapp' // assign database Name
+  host:'sh-cynosdbmysql-grp-7224u8cw.sql.tencentcdb.com',
+  port:'21364',
+  user:'voted_app_dev',
+  password:'123456',
+  database:'vote_dapp'
 }); 
 conn.connect(function(err) {
   if (err) throw err;
